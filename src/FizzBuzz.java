6e8ac72e34e91e910b021a1d2e394c4f4015dc7a
c-vanni <c.vanniasinghe@mail.utoronto.ca>
2024-09-15 22:26:18 -0400

    refactored to extract helper method

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -10,7 +10,7 @@ class FizzBuzz {
 
             // Find out which numbers divide i.
             boolean divisibleBy3 = i % 3 == 0;
-            boolean divisibleBy5 = i % 5 == 0;
+            boolean divisibleBy5 = i % 5 == 0.0;
 
             // Print our appropriate result.
             if (divisibleBy3 && divisibleBy5) {
