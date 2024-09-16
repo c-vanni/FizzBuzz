b54e600722a1c561d2d88702ddaa2d7a06345f9c
c-vanni <c.vanniasinghe@mail.utoronto.ca>
2024-09-16 00:33:43 -0400

    refactored to extract helper method

---
+++ src/Multiples.java
@@ -0,0 +1,27 @@
+public class Multiples {
+    public static void main(String[] args) {
+        //Prints how many multiples of 3 or 5 are in 1000
+        int i = 1; int total = 0;
+        while (i <= 1000/3)
+        {
+            boolean m3under1000 = (i * 3) <= 1000;
+            boolean m5under1000 = (i * 5) <= 1000;
+            boolean m15under1000 = (i * 15) <= 1000;
+
+            //Checks if there are still multiples of 3 and 5 below 1000
+            if (m3under1000 && m5under1000) {
+                total += 2;
+            }
+
+            //Checks for remaining multiples of 3
+            else if (m3under1000) {
+                total += 1;
+            }
+            if (m15under1000) {
+                total -= 1;
+            }
+            i += 1;
+        }
+        System.out.println(total);
+    }
+}
